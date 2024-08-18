07dfeaacd28eeeaeff97577f2c80be778e479705
Lootii Kiri <Lootii15@gmail.com>
2024-08-18 09:57:05 -0400

    Updates

---
+++ precache-manifest.71a8e8ea40b07c94c8b08ba1c4211bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fc0bebd1a0f3e266f4114ab561466e",
+    "url": "/SSBasketball/index.html"
+  },
+  {
+    "revision": "ce1172b01e48a1435be5",
+    "url": "/SSBasketball/static/css/main.d7498a77.chunk.css"
+  },
+  {
+    "revision": "feecbd2594daf647a62a",
+    "url": "/SSBasketball/static/js/2.b93e3a73.chunk.js"
+  },
+  {
+    "revision": "184544feb51a777f05eb9d4b59832005",
+    "url": "/SSBasketball/static/js/2.b93e3a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1172b01e48a1435be5",
+    "url": "/SSBasketball/static/js/main.dcd13929.chunk.js"
+  },
+  {
+    "revision": "25ebe57e4873b733aa60",
+    "url": "/SSBasketball/static/js/runtime-main.e76acf14.js"
+  }
+]);
